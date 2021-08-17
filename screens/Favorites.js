14d40bc596c13f1caddea082ece0ddd653954354
ahmedhassan202025 <33345323+ahmedhassan202025@users.noreply.github.com>
2021-08-17 19:24:56 +0200

    feat: add static pages

---
+++ screens/Favorites.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View } from "react-native";
+import Header from "../components/Header";
+
+const Favorites = () => {
+  return (
+    <View>
+      <Header />
+      <Text>Favorites</Text>
+    </View>
+  );
+};
+
+export default Favorites;
---
+++ screens/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View } from "react-native";
+import Header from "../components/Header";
+
+const Home = () => {
+  return (
+    <View>
+      <Header />
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+export default Home;
